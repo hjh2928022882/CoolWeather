f5386cc83d1bdb1b3b62ae0a8e2a94f3e550ec41
HJH <2928022882@qq.com>
2020-05-08 05:03:26 +0000

    add fragment

--- app/src/main/java/com/coolweather/android/util/Utility.java
+++ app/src/main/java/com/coolweather/android/util/Utility.java
@@ -11,19 +11,17 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Utility {
-    /**
-     * 解析和处理服务器返回的省级数据
-     */
+
     public static boolean handleProvinceResponse(String response) {
-        if (!TextUtils.isEmpty(response)) {  // 数据非空
+        if (!TextUtils.isEmpty(response)) {
             try {
-                JSONArray allProvinces = new JSONArray(response);   // 创建一个用于解析的 JSON 数组
+                JSONArray allProvinces = new JSONArray(response);
                 for (int i = 0; i < allProvinces.length(); i++) {
-                    JSONObject provinceObject = allProvinces.getJSONObject(i);  // 获取每一个 JSON 对象
+                    JSONObject provinceObject = allProvinces.getJSONObject(i);
                     Province province = new Province();
-                    province.setProvinceName(provinceObject.getString("name")); // 将 JSON 对象中的 name 字段赋给 ProvinceName
-                    province.setProvinceCode(provinceObject.getInt("id"));  // 将 JSON 对象中的 id 字段赋给 ProvinceCode
-                    province.save(); // 将数据存到数据库中
+                    province.setProvinceName(provinceObject.getString("name"));
+                    province.setProvinceCode(provinceObject.getInt("id"));
+                    province.save();
                 }
                 return true;
             } catch (JSONException e) {
@@ -32,9 +30,8 @@ public class Utility {
         }
         return false;
     }
-    /**
-     * 解析和处理服务器返回的市级数据
-     */
+
+
     public static boolean handleCityResponse(String response, int provinceId) {
         if (!TextUtils.isEmpty(response)) {
             try {
@@ -54,9 +51,7 @@ public class Utility {
         }
         return false;
     }
-    /**
-     * 解析和处理服务器返回的县级数据
-     */
+
     public static boolean handleCountyResponse(String response, int cityId) {
         if (!TextUtils.isEmpty(response)) {
             try {
